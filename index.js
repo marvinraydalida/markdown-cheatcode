ceecbb394f5cfdd1a66ddca5884bea6620537121
marvinraydalida <marvinraydalida@gmail.com>
2022-07-20 18:14:42 +0800

    debug

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ console.log(origin)
 const topics = ["Vanilla", "Node", "React"];
 const topicList = document.getElementById("topic-id");
 
-// fetch(origin + "/cheatcode.json")
-//     .then(response => response.json())
-//     .then(data => {
-//         topics.forEach(topic => {
-//             topicList.appendChild(createTopic());
-//             const currentTopic = topicList.lastElementChild;
-//             currentTopic.textContent = topic;
-//             currentTopic.appendChild(document.createElement('ul'));
-//             const currentTopicList = currentTopic.lastElementChild;
-//             data[topic].forEach(content => {
-//                 currentTopicList.appendChild(createContent());
-//                 currentTopicList.lastElementChild.innerHTML = `
-//                 <a href="${origin}/${topic}/${content}.md">${content}</a>
-//                 `;
-//             });
-//         });
+fetch(origin + "/cheatcode.json")
+    .then(response => response.json())
+    .then(data => {
+        topics.forEach(topic => {
+            topicList.appendChild(createTopic());
+            const currentTopic = topicList.lastElementChild;
+            currentTopic.textContent = topic;
+            currentTopic.appendChild(document.createElement('ul'));
+            const currentTopicList = currentTopic.lastElementChild;
+            data[topic].forEach(content => {
+                currentTopicList.appendChild(createContent());
+                currentTopicList.lastElementChild.innerHTML = `
+                <a href="${origin}/${topic}/${content}.md">${content}</a>
+                `;
+            });
+        });
 
-//     });
+    });
 
 
 function createTopic() {
