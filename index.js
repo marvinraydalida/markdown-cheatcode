09131571320d5127133245f840a1a1fff0af85be
marvinraydalida <marvinraydalida@gmail.com>
2022-07-20 18:02:15 +0800

    Added simple html

---
+++ index.js
@@ -0,0 +1,31 @@
+const origin = location.origin;
+const topics = ["Vanilla", "Node", "React"];
+const topicList = document.getElementById("topic-id");
+
+fetch(origin + "/cheatcode.json")
+    .then(response => response.json())
+    .then(data => {
+        topics.forEach(topic => {
+            topicList.appendChild(createTopic());
+            const currentTopic = topicList.lastElementChild;
+            currentTopic.textContent = topic;
+            currentTopic.appendChild(document.createElement('ul'));
+            const currentTopicList = currentTopic.lastElementChild;
+            data[topic].forEach(content => {
+                currentTopicList.appendChild(createContent());
+                currentTopicList.lastElementChild.innerHTML = `
+                <a href="${origin}/${topic}/${content}.md">${content}</a>
+                `;
+            });
+        });
+
+    });
+
+
+function createTopic() {
+    return document.createElement('li');
+}
+
+function createContent() {
+    return document.createElement('li');
+}
